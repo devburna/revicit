f1ee0fad3d7f8e55bd5ea0a1e993354b22f894b4
Bonaventure Umolu <bonaventure.umolu@gmail.com>
2022-09-14 11:45:14 +0100

    chore:

--- app/Http/Controllers/CompanyWalletController.php
+++ app/Http/Controllers/CompanyWalletController.php
@@ -2,17 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Enums\PaymentStatus;
-use App\Enums\PaymentType;
 use App\Http\Requests\StoreCompanyWalletRequest;
 use App\Http\Requests\StorePaymentRequest;
 use App\Http\Requests\UpdateCompanyWalletRequest;
-use App\Http\Requests\VerifyFlutterwaveTransactionRequest;
 use App\Models\CompanyWallet;
-use App\Models\Payment;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Str;
 
 class CompanyWalletController extends Controller
@@ -121,71 +115,5 @@ class CompanyWalletController extends Controller
         }
 
         return $this->show($request);
-    }
-
-    public function webHook(VerifyFlutterwaveTransactionRequest $request)
-    {
-        try {
-            return DB::transaction(function () use ($request) {
-                // verify transaction
-                $transaction = (new FlutterwaveController())->verifyTransaction($request->transaction_id);
-
-                // find wallet
-                if (!$wallet = CompanyWallet::find($transaction['data']['meta']['consumer_id'])) {
-                    throw ValidationException::withMessages(['Error occured, kindly reach out to support ASAP!']);
-                }
-
-                // checks duplicate entry
-                if (Payment::where('identity', $transaction['data']['tx_ref'])->first()) {
-                    throw ValidationException::withMessages(['Duplicate transaction found.']);
-                }
-
-                // get transaction status
-                $status = match ($transaction['data']['status']) {
-                    'success' => PaymentStatus::SUCCESSFUL(),
-                    'successful' => PaymentStatus::SUCCESSFUL(),
-                    'new' => PaymentStatus::SUCCESSFUL(),
-                    'pending' => PaymentStatus::SUCCESSFUL(),
-                    default => PaymentStatus::FAILED()
-                };
-
-                // set amount
-                $amount = match ($transaction['data']['currency']) {
-                    'USD' => $transaction['data']['amount'] * env('USD_RATE'),
-                    default => $transaction['data']['amount']
-                };
-
-                // verify it's deposit
-                match ($transaction['data']['meta']['consumer_mac']) {
-                    'deposit' => 'deposit',
-                    default => throw ValidationException::withMessages(['Error occured, kindly reach out to support ASAP!'])
-                };
-
-                // store payment
-                $storePaymentRequest = (new StorePaymentRequest($transaction));
-                $storePaymentRequest['company_wallet_id'] = $wallet->id;
-                $storePaymentRequest['identity'] = $transaction['data']['tx_ref'];
-                $storePaymentRequest['amount'] = $transaction['data']['amount'];
-                $storePaymentRequest['currency'] = $transaction['data']['currency'];
-                $storePaymentRequest['narration'] = $transaction['data']['narration'];
-                $storePaymentRequest['type'] = PaymentType::CREDIT();
-                $storePaymentRequest['status'] = $status;
-                $storePaymentRequest['meta'] = json_encode($transaction);
-                $storedTransaction = (new PaymentController())->store($storePaymentRequest);
-
-                // credit wallet if success
-                if ($storedTransaction->status->is(PaymentStatus::SUCCESSFUL()) && $wallet->credit($amount)) {
-                    return $this->show($request);
-                } else {
-                    throw ValidationException::withMessages(['Error occured, kindly reach out to support ASAP!']);
-                }
-            });
-        } catch (\Throwable $th) {
-            return response()->json([
-                'status' => false,
-                'data' => null,
-                'message' => $th->getMessage(),
-            ], 422);
-        }
     }
 }
