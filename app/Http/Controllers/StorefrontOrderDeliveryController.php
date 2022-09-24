9dc6472546e32a1fa8865c5f6e558e0a8dcc13a9
Bonaventure Umolu <bonaventure.umolu@gmail.com>
2022-09-24 13:13:22 +0100

    chore:

---
+++ app/Http/Controllers/StorefrontOrderDeliveryController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreStorefrontOrderDeliveryRequest;
+use App\Http\Requests\UpdateStorefrontOrderDeliveryRequest;
+use App\Models\StorefrontOrderDelivery;
+
+class StorefrontOrderDeliveryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreStorefrontOrderDeliveryRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreStorefrontOrderDeliveryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\StorefrontOrderDelivery  $storefrontOrderDelivery
+     * @return \Illuminate\Http\Response
+     */
+    public function show(StorefrontOrderDelivery $storefrontOrderDelivery)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\StorefrontOrderDelivery  $storefrontOrderDelivery
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(StorefrontOrderDelivery $storefrontOrderDelivery)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateStorefrontOrderDeliveryRequest  $request
+     * @param  \App\Models\StorefrontOrderDelivery  $storefrontOrderDelivery
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateStorefrontOrderDeliveryRequest $request, StorefrontOrderDelivery $storefrontOrderDelivery)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\StorefrontOrderDelivery  $storefrontOrderDelivery
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(StorefrontOrderDelivery $storefrontOrderDelivery)
+    {
+        //
+    }
+}
